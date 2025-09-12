1d569acbc5715c87cbae0e0c9d8abeb2ceca8dec
davidatkinsonphotography-max <davidatkinson.photography@gmail.com>
2025-09-12 10:40:37 +1000

    Update wsgi.py

--- summerfest/wsgi.py
+++ summerfest/wsgi.py
@@ -11,18 +11,18 @@ import os
 import sys
 
 # Add your project directory to the Python path
-path = '/home/atkinsondp/summerfest_registration'
-if path not in sys.path:
-    sys.path.append(path)
+project_home = '/home/atkinsondp/summerfest_registration'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
-# Set the settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'summerfest.settings')
+# Set environment variable to tell Django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'summerfest.settings'
 
 # Activate your virtualenv
 activate_this = '/home/atkinsondp/summerfest_registration/venv/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
-# Import Django application
+# Serve Django via WSGI
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

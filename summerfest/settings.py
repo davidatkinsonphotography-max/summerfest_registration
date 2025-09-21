13bcd099c696465074fc661bd6dc35cc8361e2b4
davidatkinsonphotography-max <davidatkinson.photography@gmail.com>
2025-09-21 17:29:16 +1000

    Update settings.py

--- summerfest/settings.py
+++ summerfest/settings.py
@@ -13,6 +13,8 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
